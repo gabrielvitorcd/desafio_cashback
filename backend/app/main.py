6b877cb15a29ea9b01cd5f04a4984fc5dc61aac5
Gabriel Vitor <gabriel.vitor03@gmail.com>
2026-04-17 14:05:03 -0300

    fix: adiciona domínio Vercel no CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,10 @@ app = FastAPI(
 # ← adiciona esse bloco
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://desafio-cashback-pied.vercel.app/"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

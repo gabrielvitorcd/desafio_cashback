9e578a5c4220303185dab4aa5061ff40aed9cb95
Gabriel Vitor <gabriel.vitor03@gmail.com>
2026-04-17 13:02:22 -0300

    feat(REACT): create components de consulta e historico com module css separado do global e do app principal, estrutura base do html foi mantida no global

---
+++ frontend/src/components/ConsultaPreco.tsx
@@ -0,0 +1,72 @@
+import { useCashback } from "../hooks/useCashback";
+import { Historico } from "./Historico";
+
+import styles from "./ConsultaPreco.module.css";
+
+export function ConsultaPreco() {
+  const { form, atualizarForm, resultado, historico, loading, erro, calcular } =
+    useCashback();
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>💸 Calculadora de Cashback</h1>
+
+      <div className={styles.form}>
+        <input
+          className="form-input"
+          type="number"
+          placeholder="Valor da compra"
+          value={form.valor}
+          onChange={(e) => atualizarForm("valor", e.target.value)}
+        />
+
+        <input
+          className="form-input"
+          type="number"
+          placeholder="Desconto (%)"
+          value={form.desconto}
+          onChange={(e) => atualizarForm("desconto", e.target.value)}
+        />
+
+        <div className={styles["tipo-cliente-group"]}>
+          <button
+            type="button"
+            className={`${styles["btn-opcao"]} ${form.tipoCliente === "Normal" ? "active" : ""}`}
+            onClick={() => atualizarForm("tipoCliente", "Normal")}
+          >
+            Cliente Normal
+          </button>
+
+          <button
+            type="button"
+            className={`${styles["btn-opcao"]} ${form.tipoCliente === "Vip" ? "active" : ""}`}
+            onClick={() => atualizarForm("tipoCliente", "Vip")}
+          >
+            Cliente VIP
+          </button>
+        </div>
+
+        <button onClick={calcular} disabled={loading}>
+          {loading ? "Calculando..." : "Calcular Cashback"}
+        </button>
+      </div>
+
+      {erro && <p style={{ color: "red" }}>{erro}</p>}
+
+      {resultado && (
+        <div className={styles.result}>
+          <p>Valor final: R$ {resultado.valor_final.toFixed(2)}</p>
+          <p className={styles.cashback}>
+            Cashback: R$ {resultado.valor_cashback.toFixed(2)}
+          </p>
+          {resultado.valor_compra > 500 && (
+            <span className={styles["badge-promo"]}>Cashback Dobrado!</span>
+          )}
+        </div>
+      )}
+
+      {/* Histórico */}
+      <Historico dados={historico || []} />
+    </div>
+  );
+}
---
+++ frontend/src/components/Historico.tsx
@@ -0,0 +1,66 @@
+import { type ResultadoCashback } from "../hooks/useCashback";
+import styles from "./Historico.module.css";
+
+interface HistoricoProps {
+  dados: ResultadoCashback[];
+}
+
+export function Historico({ dados }: HistoricoProps) {
+  const formatarDevice = (userAgent: string) => {
+    if (!userAgent) return "Desconhecido";
+    const ua = userAgent.toLowerCase();
+    let sistema = "Desconhecido";
+    let navegador = "Navegador";
+
+    if (ua.includes("win")) sistema = "Windows";
+    else if (ua.includes("mac")) sistema = "MacOs";
+    else if (ua.includes("linux")) sistema = "Linux";
+    else if (ua.includes("android")) sistema = "Android";
+    else if (ua.includes("iphone") || ua.includes("ipad")) sistema = "iOS";
+
+    if (ua.includes("firefox")) navegador = "Firefox";
+    else if (ua.includes("edg")) navegador = "Edge";
+    else if (ua.includes("chrome")) navegador = "Chrome";
+    else if (ua.includes("safari")) navegador = "Safari";
+
+    return `${navegador} (${sistema})`;
+  };
+
+  return (
+    <div className={styles.history}>
+      <h2>Histórico</h2>
+      <div className={styles.historyList}>
+        {dados.map((item) => (
+          <div key={item.id} className={styles.historyItem}>
+            <p className={styles.deviceInfo}>
+              <strong>Device:</strong> {formatarDevice(item.device)}
+            </p>
+
+            <p>IP: {item.ip}</p>
+            <p>Tipo: {item.tipo_cliente}</p>
+            <p>Valor: R$ {item.valor_compra.toFixed(2)}</p>
+            <p>Desconto: {item.percentual_desconto}%</p>
+
+            <div className={styles.statusCashback}>
+              {item.valor_compra > 500 ? (
+                <span className={`${styles.badgeResult} ${styles.premium}`}>
+                  🚀 Cashback Dobrado!
+                  <p className={styles.cashbackValue}>
+                    R$ {item.valor_cashback.toFixed(2)}
+                  </p>
+                </span>
+              ) : (
+                <span className={`${styles.badgeResult} ${styles.comum}`}>
+                  ✅ Cashback Padrão
+                  <p className={styles.cashbackValue}>
+                    R$ {item.valor_cashback.toFixed(2)}
+                  </p>
+                </span>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
